4454bc7d9745e120469de38cc43e558dbcefb5ec
Maxime Rose <maxime.r80@gmail.com>
2020-11-21 12:59:49 +0100

    wip

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 document.onreadystatechange = () => {
     if (document.readyState === 'complete') {
-        let body = document.getElementsByTagName('body')[0];
         let button = document.getElementById('menu-toggler');
         let burger = document.getElementById('burger-icon');
         let mobileMenu = document.getElementById('mobile-menu');
@@ -9,7 +8,6 @@ document.onreadystatechange = () => {
             burger.classList.toggle('active');
             button.classList.toggle('rotate');
             mobileMenu.classList.toggle('active');
-            body.classList.toggle('menu-open');
         }
     }
 };
