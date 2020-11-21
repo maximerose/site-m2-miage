e688673c29b01244280dce8d70559c3a7f3c5330
Maxime Rose <maxime.r80@gmail.com>
2020-11-21 02:06:07 +0100

    Burger menu

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 document.onreadystatechange = () => {
     if (document.readyState === 'complete') {
+        let main = document.getElementById('main');
         let button = document.getElementById('menu-toggler');
+        let burger = document.getElementById('burger-icon');
         let menu = document.getElementById('main-menu');
 
         button.onclick = () => {
-
+            burger.classList.toggle('active');
+            button.classList.toggle('rotate');
+            menu.classList.toggle('active');
+            main.classList.toggle('darker');
         }
     }
 };
