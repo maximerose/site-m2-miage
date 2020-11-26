173f757ca4bfcb6ce68d9186e783ca5ffd7f995b
maxim <M1D1M@nson>
2020-11-26 17:19:31 +0100

    Carousel

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+const INTERVAL = 40;
+
 document.onreadystatechange = () => {
     if (document.readyState === 'complete') {
         let button = document.getElementById('menu-toggler');
@@ -9,5 +11,99 @@ document.onreadystatechange = () => {
             button.classList.toggle('rotate');
             mobileMenu.classList.toggle('active');
         }
+
+        let carousel = document.getElementById('carousel');
+        let items = carousel.getElementsByClassName('carousel-item');
+        let nbImages = items.length - 1;
+        let indicatorsUl = document.getElementById('carousel-indicators');
+        let indicators = indicatorsUl.getElementsByTagName('li');
+        let previous = document.getElementById('carousel-prev');
+        let next = document.getElementById('carousel-next');
+
+        let activeItem;
+        let activeIndicator;
+
+        let getActiveElements = () => {
+            let activeElements = {};
+            for (let i = 0; i < items.length; i++) {
+                if (items[i].classList.contains('active')) {
+                    activeElements.item = items[i];
+                }
+
+                if (indicators[i].classList.contains('active')) {
+                    activeElements.indicator = indicators[i];
+                }
+            }
+
+            return activeElements;
+        }
+
+        let displayImage = (index) => {
+            activeItem = getActiveElements().item;
+            activeItem.classList.toggle('active');
+
+            activeIndicator = getActiveElements().indicator;
+            activeIndicator.classList.toggle('active');
+
+            if (!items[index].classList.contains('active')) {
+                items[index].classList.add('active');
+            }
+
+            if (!indicators[index].classList.contains('active')) {
+                indicators[index].classList.add('active');
+            }
+        };
+
+
+        let slide = (direction) => {
+            activeItem = getActiveElements().item;
+            activeIndicator = getActiveElements().indicator;
+
+            if (activeItem && activeIndicator) {
+                let index = activeItem.dataset.item;
+
+                activeItem.classList.toggle('active');
+                activeIndicator.classList.toggle('active');
+
+                if (direction < 0) {
+                    index--;
+                    if (index < 0) {
+                        items[nbImages].classList.toggle('active');
+                        indicators[nbImages].classList.toggle('active');
+                    } else {
+                        items[index].classList.toggle('active');
+                        indicators[index].classList.toggle('active');
+                    }
+                } else {
+                    index++;
+                    if (index > nbImages) {
+                        items[0].classList.toggle('active');
+                        indicators[0].classList.toggle('active');
+                    } else {
+                        items[index].classList.toggle('active');
+                        indicators[index].classList.toggle('active');
+                    }
+                }
+            }
+
+            activeItem = getActiveElements().item;
+            activeIndicator = getActiveElements().indicator;
+        }
+
+        let indicatorsLi = document.querySelectorAll('.carousel-indicators li');
+
+        indicatorsLi.forEach((li) => {
+            li.addEventListener('click', (event) => {
+                displayImage(event.target.dataset.slideTo);
+            });
+        });
+
+        previous.onclick = () => {
+            slide(-1);
+        };
+
+        next.onclick = () => {
+            slide(1);
+        };
     }
 };
